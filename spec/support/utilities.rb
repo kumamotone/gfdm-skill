725534acc4243e4be88be8adc7103927cb444dd5
Kazumasa KUMAMOTO <kumamotone@gmail.com>
2015-06-14 16:12:14 +0900

    chap5

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,1 @@
-def full_title(page_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
+include ApplicationHelper
