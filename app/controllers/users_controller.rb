ba17921f593115971d54954e0801328bba0f5ac2
Kazumasa KUMAMOTO <kumamotone@gmail.com>
2015-06-22 02:23:57 +0900

    modify music numbers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @hot = @user.skills.where(music_id: 1..711)
-    @other = @user.skills.where(music_id: 712..756) # 終端位置変更の必要あり
+    @hot = @user.skills.where(music_id: 712..756)
+    @other = @user.skills.where(music_id: 1..711) # 終端位置変更の必要あり
     #@user.skills.order("sp DESC").each do |s|
     #  music = Music.find(s.music_id)
     #  if music.ishot
@@ -19,14 +19,14 @@ class UsersController < ApplicationController
     #  end
     #end
     
-    hot_limit = @user.skills.where(music_id: 1..711).order("sp DESC").limit(25)
+    hot_limit = @user.skills.where(music_id: 712..756).order("sp DESC").limit(25)
     @hot_sp = 0.0
 
     hot_limit.each do |h|
       @hot_sp = @hot_sp + h.sp
     end
     
-    other_limit = @user.skills.where(music_id: 712..756).order("sp DESC").limit(25)
+    other_limit = @user.skills.where(music_id: 1..711).order("sp DESC").limit(25)
     @other_sp = 0.0
 
     other_limit.each do |o|
