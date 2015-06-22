3bf4883e63b116150171db57802bd6a68545ba95
Kazumasa KUMAMOTO <kumamotone@gmail.com>
2015-06-23 04:13:56 +0900

    UIの微調整

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -26,7 +26,7 @@ module SessionsHelper
   def signed_in_user
     unless signed_in?
       store_location
-      redirect_to signin_url, notice: "Please sign in."
+      redirect_to signin_url, notice: "ログインしてください．"
     end
   end
 
