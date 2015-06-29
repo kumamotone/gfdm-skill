80c8e44c526255f69ee140c1e3128fd622f42875
Kazumasa KUMAMOTO <kumamotone@gmail.com>
2015-06-29 17:09:40 +0900

    add: developmentのメール設定をproductionに反映

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,7 @@ SampleApp::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
   config.action_mailer.default_url_options = { :host => 'localhost:3000' } 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,15 @@ SampleApp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  config.action_mailer.default_url_options = { :host => 'tri.gfdm-skill.net' } 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => ENV['GMAIL_ID'],  
+    :password => ENV['GMAIL_PASS'],
+    :authentication => 'plain',
+    :enable_starttls_auto => true,
+  }
 end
