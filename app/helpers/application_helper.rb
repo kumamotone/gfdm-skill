ff0a65deac060a140d88fcb03a0a5b76b05cff61
Kazumasa KUMAMOTO <kumamotone@gmail.com>
2015-07-03 04:06:12 +0900

    modify:サイトタイトル変更 TOPにログインと新規登録リンク追加 外部リンクはtarget=_blankに変更 refs #1 #2 #3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title)
-    base_title = "GITADORA Tri-boost Skill Simulator"
+    base_title = "GITADORA Tri-boost スキルシミュレータ"
     if page_title.empty?
       base_title
     else
