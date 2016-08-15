b0d6eb3ae0797fc64fbcfe288700c29a98007626
Kazumasa KUMAMOTOF29 <kumamotone@gmail.com>
2016-08-15 18:58:05 +0900

    Music にバリデーションを追加

--- app/models/music.rb
+++ app/models/music.rb
@@ -1,3 +1,18 @@
 class Music < ActiveRecord::Base
   has_many :skill, dependent: :destroy
+  validates :name, :ishot, presence: true
+  validates :d_bsc, presence: true, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 9.99}
+  validates :d_adv, presence: true, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 9.99}
+  validates :d_ext, presence: true, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 9.99}
+  validates :d_mas, presence: true, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 9.99}
+
+  validates :g_bsc, presence: true, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 9.99}
+  validates :g_adv, presence: true, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 9.99}
+  validates :g_ext, presence: true, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 9.99}
+  validates :g_mas, presence: true, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 9.99}
+
+  validates :b_bsc, presence: true, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 9.99}
+  validates :b_adv, presence: true, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 9.99}
+  validates :b_ext, presence: true, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 9.99}
+  validates :b_mas, presence: true, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 9.99}
 end
