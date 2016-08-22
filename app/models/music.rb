9f72abeb7461b6ad7789b3cf034d97531897327d
Kazumasa KUMAMOTOF29 <kumamotone@gmail.com>
2016-08-22 18:49:45 +0900

    Music の isHot の バリデーションを変更

--- app/models/music.rb
+++ app/models/music.rb
@@ -1,6 +1,6 @@
 class Music < ActiveRecord::Base
   has_many :skill, dependent: :destroy
-  validates :name, :ishot, presence: true
+  validates_inclusion_of :ishot, :in => [true, false]
   validates :d_bsc, presence: true, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 9.99}
   validates :d_adv, presence: true, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 9.99}
   validates :d_ext, presence: true, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 9.99}
